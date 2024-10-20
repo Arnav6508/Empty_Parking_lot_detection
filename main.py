e9c91b7fa11cd7bb37c3c97d9b843337416f43f3
Arnav6508 <arnavgupta6508@gmail.com>
2024-10-20 19:21:18 +0530

    added empty parking space counter

--- main.py
+++ main.py
@@ -16,17 +16,21 @@ ret = True
 frame_nmr = 0
 steps = 30
 spots_status = [0 for j in spots]
+total_spots = 396
+empty_spots = 0
 
 while ret:
     ret, frame = cap.read()
 
     if frame_nmr % steps == 0:
+        empty_spots = 0
         for spot_idx, spot in enumerate(spots):
             x1, y1, w, h = spot
             spot_crop = frame[y1:y1+h, x1:x1+w, :]
 
             spot_status = isEmpty(spot_crop)
             spots_status[spot_idx] = spot_status
+            empty_spots += spot_status
 
         prev_frame = frame.copy()
 
@@ -40,6 +44,9 @@ while ret:
             frame = cv2.rectangle(frame, (x1, y1), (x1+w, y1+h), (0, 0, 255), 2)
 
     frame_nmr += 1
+
+    cv2.rectangle(frame, (15, 25), (440, 70), (0, 0, 0), -1)
+    cv2.putText(frame, f'Available spots: {empty_spots}/{total_spots}', (30,60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     cv2.imshow('frame', frame)
